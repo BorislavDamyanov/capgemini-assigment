38f0aeb6c2cee3fc19024bfe5a66aa3644d5a694
boris.damyanov <boris.damyanov@gmail.com>
2022-12-19 10:47:35 +0100

    Initial Commit

---
+++ src/main/java/com/example/capgeminiassigment/CapgeminiAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.capgeminiassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapgeminiAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capgeminiassigment/CapgeminiAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capgeminiassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgeminiAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
